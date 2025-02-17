d3f3e69344d8c7327874518c75f378d70598d861
Trevor Jennings <TrevorJennings@bcs.org>
2025-02-17 17:30:23 +0000

    Rxploring and categorising errors reported by the Examiner.

--- explore/explore.ads
+++ explore/explore.ads
@@ -1,13 +1,30 @@
 pragma SPARK_Mode;
 package Explore is
    procedure UE1 (I : in out Integer);
+   --  Unconditional data-flow errors 20, 23.
+   --  Suplimentary error 32 - Neither imported or defined.
+   --  Dependencies error 602 - Exported value derived from uninitialized local.
    procedure CE1 (I : in out Integer);
+   --  Conditional deata-flow errors 501, 504.
+   --  Dependencies error 602 - Exported value derived from uninitialized local.
    procedure BE1 (I : in out Integer);
+   --  Blocking statements
+   --  Unconditional data-flow errors 20.
+   --  Dependencies error 602 - Exported value derived from uninitialized local.
    procedure UA1 (I : in out Integer);
+   --  Unconditional data-flow anomally 10 - Ineffective statement.
    procedure UA2 (I : in out Integer);
+   --  Ineffective statement in loop.
+   --  Unconditional data-flow anomally 10 - Ineffective statement.
    procedure UA3 (I : in out Integer);
+   --  Ineffective statement before loop entry.
+   --  Unconditional data-flow anomally 10 - Ineffective statement.
    procedure UA4 (I : in out Integer);
+   --  Ineffective statement before loop entry containing if-then-else.
+   --  Unconditional data-flow anomally 10 - Ineffective statement.
    procedure UA5 (I : in out Integer);
+
+   --  Unconditional data-flow anomally 10 - Ineffective statement.
    procedure UA6 (I : in out Integer);
    procedure UA7 (I : in out Integer);
    procedure I1  (I : in out Integer);
