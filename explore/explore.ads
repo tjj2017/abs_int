8076f1ecf7c05f944dc379817390bc664b72c9ef
Trevor Jennings <TrevorJennings@bcs.org>
2025-02-24 18:12:17 +0000

    Add more info to explore subprograms.

--- explore/explore.ads
+++ explore/explore.ads
@@ -1,4 +1,4 @@
-pragma SPARK_Mode;
+0pragma SPARK_Mode;
 package Explore is
    procedure UE1 (I : in out Integer);
    --  Unconditional data-flow errors 20, 23.
@@ -23,11 +23,20 @@ package Explore is
    --  Ineffective statement before loop entry containing if-then-else.
    --  Unconditional data-flow anomally 10 - Ineffective statement.
    procedure UA5 (I : in out Integer);
-
+   --  Ineffective value returned from subprogram call
    --  Unconditional data-flow anomally 10 - Ineffective statement.
+   --  Unconditional data-flow anomally 10 - Assignment to V is ineffective.
    procedure UA6 (I : in out Integer);
+   --  Ineffective blocking statement
+   --  Unconditional data-flow anomally 10 - Ineffective statement.
    procedure UA7 (I : in out Integer);
+   --  Ineffective initialization due to blocking statement.
+   --  Unconditional data-flow anomally 54 - The initialization at declaration of V is ineffective.
    procedure I1  (I : in out Integer);
+   --  Invariant conditions
+   --  Flow-Anomally  22 - Value of expression is invariant.
+   --  Flow-Anomally  40 - Exit condition is stable, of index 0.
+   --  Flow-Anomally  40 - Exit condition is stable, of index 1.
    procedure I2  (I : in out Integer);
    procedure IO1 (I : in out Integer);
    procedure IO2 (I : in out Integer; J : in Integer);
